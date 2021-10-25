ab143d2e9ed5f27a4e1cf2b156e0416815911d06
misha1997 <mishaotroshenko2013@gmail.com>
2021-10-25 21:03:00 +0300

    adaptive

---
+++ js/modal.js
@@ -0,0 +1,26 @@
+var modal = false;
+function showHideModal() {
+  modal = !modal;
+  if(modal) {
+    document.getElementById('modal').style.display = "flex";
+    document.getElementsByTagName('body')[0].style.overflow = "hidden";
+  } else {
+    document.getElementById('modal').style.display = "none";
+    document.getElementsByTagName('body')[0].style.overflow = "auto";
+  }
+}
+
+var menuToogle = false;
+var menuToogleBtn = document.getElementById('menu-toggle');
+menuToogleBtn.addEventListener("click", function() {
+  menuToogle = !menuToogle;
+  if(menuToogle) {
+    document.getElementById('menubox').style.display = "block";
+  } else {
+    document.getElementById('menubox').style.display = "none";
+  }
+});
+
+function myFunction() {
+  document.getElementById("dropdown").classList.toggle("active");
+}
---
+++ js/slidersConfig.js
@@ -0,0 +1,44 @@
+// rewievs
+var swiper = new Swiper(".rewievsSwiper", {
+  slidesPerView: 3,
+  spaceBetween: 30
+});
+
+// instruments
+var swiper = new Swiper(".instrumentsSwiper", {
+  slidesPerView: 4,
+  spaceBetween: 30
+});
+
+// certiifcate
+var swiper = new Swiper(".certiifcateSwiper", {
+  slidesPerView: 4,
+  spaceBetween: 30,
+  pagination: {
+    el: ".swiper-pagination",
+  },
+});
+
+// porffolio slider
+var swiper = new Swiper(".portfolioSwiper", {
+  slidesPerView: 5,
+  spaceBetween: 14,
+  scrollbar: {
+    el: ".swiper-scrollbar",
+    hide: false,
+  },
+});
+currentSlide(0);
+function currentSlide(n) {
+  var slides = document.getElementsByClassName("portfolio-item");
+  var buttons = document.getElementsByClassName(" portfolio-button");
+  for (let i = 0; i < slides.length; i++) {
+    if(i == n) {
+      slides[i].style.display = "grid";
+      buttons[i].classList.add('active');
+    } else {
+      slides[i].style.display = "none";
+      buttons[i].classList.remove('active');
+    }
+  }
+}
